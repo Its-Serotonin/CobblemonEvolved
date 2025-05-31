5d9c2a1da6d0c45a2a03b56a5486d7b723f96e24
Serotonin/Elliott <elliottbrown6585@gmail.com>
2025-05-31 06:41:59 -0500

    Remove unusedassets and unusedscreenshotshit from tracking

--- unusedassets/SUPA/FeatureAccessBridge.java
+++
@@ -1,9 +0,0 @@
-package com.serotonin.common.merchant;
-
-import net.minecraft.client.render.entity.feature.FeatureRenderer;
-import java.util.List;
-
-
-public interface FeatureAccessBridge {
-    List<FeatureRenderer<?, ?>> cobblemonEvolved$getFeatures();
-}
